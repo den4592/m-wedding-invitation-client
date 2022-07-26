8225364a0736d88deb4cf9bb73e490bfab4b24c6
ILYA <ilya@ILYAs-MacBook-Pro.local>
2022-07-26 19:35:16 +0900

    reverse guest-text

--- src/components/GuestData.js
+++ src/components/GuestData.js
@@ -1,13 +1,16 @@
 const GuestData = ({ guestData }) => {
-  const td = guestData.map((data) => {
-    return (
-      <div key={data.id} className="guest-box">
-        <p className="from-text">From.{data.name}</p>
-        <hr />
-        <p className="text-text">{data.text}</p>
-      </div>
-    );
-  });
+  const td = guestData
+    .slice(0)
+    .reverse()
+    .map((data) => {
+      return (
+        <div key={data.id} className="guest-box">
+          <p className="from-text">From.{data.name}</p>
+          <hr />
+          <p className="text-text">{data.text}</p>
+        </div>
+      );
+    });
 
   return <div className="guest-data">{td}</div>;
 };
