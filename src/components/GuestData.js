e4c0501d4eac69f7c0130e7952e0d1df64c13dac
ILYA <ilya@ILYAs-MacBook-Pro.local>
2022-07-26 13:03:28 +0900

    guestbox

--- src/components/GuestData.js
+++ src/components/GuestData.js
@@ -1,9 +1,10 @@
 const GuestData = ({ guestData }) => {
   const td = guestData.map((data) => {
     return (
-      <div key={data.id}>
-        <p>{data.name}</p>
-        <p>{data.text}</p>
+      <div key={data.id} className="guest-box">
+        <p className="from-text">From.{data.name}</p>
+        <hr />
+        <p className="text-text">{data.text}</p>
       </div>
     );
   });
